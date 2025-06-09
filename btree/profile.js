9a42d3fb0312c37b048c911245b52b3afdecb8f8
lukas8219 <lucas.c4d@gmail.com>
2025-06-08 22:10:55 -0300

    started optimizing for real

---
+++ btree/profile.js
@@ -0,0 +1,14 @@
+import { DiskBPlusTree } from './optimized.js';
+
+const name = 'optimized';
+const newTree = new DiskBPlusTree(name)
+
+const keysWithDifferentValues = new Set([1, 1000, 5000, 120_000, 333_333, 500_000, 888_000, 999_999])
+const MOCK_VALUES = 4294967295;
+const valueToBeTested = 999;
+
+const indexes = [...Array(thresholds.insert.keys).keys()].map((i) => keysWithDifferentValues.has(i) ? valueToBeTested : MOCK_VALUES);
+for(const [index, value] of indexes.entries()){
+  newTree.insert(index,value)
+}
+newTree.flush();

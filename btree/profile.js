ef48ebbff96269ebaaf4a849385d2406d4ae58f5
lukas8219 <lucas.c4d@gmail.com>
2025-06-10 21:48:54 -0300

    addded more commands

--- btree/profile.js
+++ btree/profile.js
@@ -2,14 +2,13 @@ import { DiskBPlusTree } from './optimized.js';
 import fs from 'fs'
 
 const name = 'optimized';
-const newTree = new DiskBPlusTree(name)
-
 const keysWithDifferentValues = new Set([1, 1000, 5000, 120_000, 333_333, 500_000, 888_000, 999_999])
 const MOCK_VALUES = 4294967295;
 const valueToBeTested = 999;
-
 const indexes = [...Array(1_000_000).keys()].map((i) => keysWithDifferentValues.has(i) ? valueToBeTested : MOCK_VALUES);
+
 try {
+  const newTree = new DiskBPlusTree(name)
   for(const [index, value] of indexes.entries()){
     newTree.insert(index,value)
   }
